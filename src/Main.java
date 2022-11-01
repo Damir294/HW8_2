7232d24869ad99c3ef73b248d8b9534017df7f52
Дамир <damir.obuhov@mail.ru>
2022-11-02 01:53:59 +0400

    решил

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+         byte a= 27;
+         byte b=23;
+         byte c =30;
+         int d =480;
+         int e=(a+b+c);
+         int r= d / e;
+         int q= a*r;
+         int w = c*r;
+         int t= b*r;
+        System.out.println(q);
+        System.out.println(w);
+        System.out.println(t);
+        System.out.println( "На каждого учкника расчитанно "+ r + " Листов бумаги ");
+    }
+}
